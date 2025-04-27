9549f63fc39b17819767e17215d88c7a777616ac
Sebastian Lech <lechsebastian99@gmail.com>
2025-04-27 18:21:34 +0200

    Update isar_flutter_libs dependency to git source due to build issues

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/pages/new_note_page.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,8 +9,25 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  // CREATE
+  void createNote() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const NewNotePage()));
+  }
+
+  // READ
+  void readNotes() {}
+
+  // UPDATE
+
+  // DELETE
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
