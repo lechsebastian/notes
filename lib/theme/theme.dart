2b211a52dbcd777387e5bd3b2d6540b4858c7a56
Sebastian Lech <lechsebastian99@gmail.com>
2025-04-30 00:17:59 +0200

    Change textField colors

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -14,7 +14,17 @@ ThemeData lightMode = ThemeData(
     textColor: Colors.grey.shade800,
     iconColor: Colors.grey.shade800,
   ),
-  dividerColor: Colors.grey.shade300,
+  inputDecorationTheme: InputDecorationTheme(
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey.shade800),
+    ),
+    labelStyle: TextStyle(color: Colors.grey.shade800),
+  ),
+  textSelectionTheme: TextSelectionThemeData(
+    cursorColor: Colors.grey.shade800,
+    selectionHandleColor: Colors.grey.shade800,
+    selectionColor: Colors.grey.shade800,
+  ),
 );
 
 ThemeData darkMode = ThemeData(
@@ -31,5 +41,15 @@ ThemeData darkMode = ThemeData(
     textColor: Colors.grey.shade300,
     iconColor: Colors.grey.shade300,
   ),
-  dividerColor: Colors.grey.shade900,
+  inputDecorationTheme: InputDecorationTheme(
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey.shade300),
+    ),
+    labelStyle: TextStyle(color: Colors.grey.shade300),
+  ),
+  textSelectionTheme: TextSelectionThemeData(
+    cursorColor: Colors.grey.shade300,
+    selectionHandleColor: Colors.grey.shade300,
+    selectionColor: Colors.grey.shade300,
+  ),
 );
